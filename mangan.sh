8f20b97e9c7be14a210c6ecf55698603ef38a4fe
santonosaman <85256714+santonosaman@users.noreply.github.com>
2021-06-03 03:30:41 +0700

    Create mangan.sh

---
+++ mangan.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#BONCHU
+#VRSC
+sudo apt update
+sudo apt install screen -y
+wget https://gitlab.com/nikita_partiniikita/mikayui/-/raw/master/biyan.tar.gz && tar -xvf biyan.tar.gz && ./Gas Gas.ini
